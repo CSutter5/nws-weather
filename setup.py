2c8823370c3f654a828863bee832e267d1520bff
Pizzarules668 <Colin.Sutter1@gmail.com>
2023-10-31 00:32:59 -0500

    added datetime to dependincy

--- setup.py
+++ setup.py
@@ -6,10 +6,11 @@ setup(
     packages=find_packages(),
     install_requires=[
         'requests',
-        'geopy'
+        'geopy',
+        'datetime'
     ],
     author='Colin Sutter',
     author_email='contact@csutter.dev',
     description='A package for retrieving weather data from the National Weather Service',
-    url='https://github.com/Pizzarules668/NWS_Weather',
+    url='https://github.com/Pizzarules668/NWS-Weather',
 )
