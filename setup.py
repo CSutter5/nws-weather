e572c725c614a38c6d61ad9453d79ac5aac9f0ef
Pizzarules668 <Colin.Sutter1@gmail.com>
2023-12-18 17:07:08 -0600

    Changed version to 1.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='NWS_Weather',
-    version='1.1.0',
+    version='1.1.1',
     packages=find_packages(),
     install_requires=[
         'requests',
